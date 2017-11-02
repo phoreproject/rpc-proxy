69e74de2936da932671b10a4cbdc16405d7f9014
Julian Meyer <julianmeyer2000@gmail.com>
2017-11-02 14:35:50 -0700

    Fix CORS

--- proxy.py
+++ proxy.py
@@ -57,6 +57,7 @@ def rpc_proxy_class(config_data):
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.send_header('Content-Length', str(len(data)))
+            self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
             self.wfile.write(data)
 
