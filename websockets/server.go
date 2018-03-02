01ec3ddec9091c38ea73875b371969f2c6517d3e
Julian Meyer <julianmeyer2000@gmail.com>
2018-03-01 22:22:31 -0800

    Update buffer size to allow for larger messages

--- websockets/server.go
+++ websockets/server.go
@@ -23,8 +23,6 @@ const (
 	pongWait = 60 * time.Second
 	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = (pongWait * 9) / 10
-	// Maximum message size allowed from peer.
-	maxMessageSize = 512
 )
 
 var (
@@ -33,8 +31,8 @@ var (
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+	ReadBufferSize:  1048576,
+	WriteBufferSize: 1048576,
 }
 
 // Client is a middleman between the websocket connection and the hub.
